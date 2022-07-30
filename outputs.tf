201d9f9b3fe6b8a6b840ff545d083df3f896007c
Tell Me With Data Admin <tell.me.with.data@gmail.com>
2022-07-29 22:20:03 -0500

    Create resource groups for a sales datalake

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "resource_group_sales_dev" {
+  value = azurerm_resource_group.resource_group_sales_dev.name
+}
+
+output "resource_group_sales_prod" {
+  value = azurerm_resource_group.resource_group_sales_prod.name
+}
